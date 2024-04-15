3becfefc65f7cd83fa27f663779f163e113e88fd
Abdul Ahad Hassan <malikabdulahadhassan@gmail.com>
2024-04-15 22:19:40 +0500

    js6

---
+++ app.js
@@ -0,0 +1,30 @@
+// chapter 21-25
+
+// Q1
+// var firstName = prompt('Enter ur firrst name')
+// var lastName = prompt('Enter ur last name')
+// var fullName = firstName + ' ' + lastName
+// document.write(`greetings ${fullName}` )
+
+
+// Q2
+// var userImp = prompt('enter ur fav mobile modal') 
+// document.write(`My fav phone is: ${userImp} <br>`)
+// document.write(`String length: ${userImp.length} `)
+
+// Q3
+// var arr = "pakistan"
+// var n = 'n'
+// document.write(`string: ${arr} <br>  Index of ${n}: ${arr.indexOf(n)}`)
+
+// q4
+// var arr = "hello world"
+// var n = 'l'
+// document.write(`string: ${arr} <br> Last Index of ${n}: ${arr.lastIndexOf(n)}`)
+
+// Q5
+// var arr = "pakistan"
+// var n = 3
+// document.write(`string: ${arr} <br>  Character at index ${n}: ${arr[n]}`)
+
+// Q6
